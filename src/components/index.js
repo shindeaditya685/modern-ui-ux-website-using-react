72d9cdccefb68bd964033a3ae6cda84238f12aee
Aditya Shinde <shindeaditya685@gmail.com>
2024-03-05 18:13:33 +0530

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Feedback from "./FeedbackCard";
+import Stats from "./Stats";
+import GetStarted from "./GetStarted";
+import Tesimonials from "./Tesimonials";
+import Hero from "./Hero";
+import Footer from "./Footer";
+
+export {
+  Navbar,
+  Billing,
+  Business,
+  CardDeal,
+  Clients,
+  CTA,
+  Stats,
+  Hero,
+  Tesimonials,
+  Footer,
+};
